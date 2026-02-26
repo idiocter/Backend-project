a5bfdba243233ccfabdb187848637bcd6af75d77
Bipul Regmi <bipulregmi2018@gmail.com>
2026-02-26 18:40:32 +0545

    add cloudinary and mutler

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 import bycrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config()
 const userSchema = new mongoose.Schema({
     
     username:{
